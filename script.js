66f4d9bd2340fd4947b0207bb04edeaeb1dd44cf
eugehvicz <geovanna.pazini@aluno.senai.br>
2025-06-17 17:22:32 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const peso = document.getElementById("peso")
-const imcForm = document.getElementById("imcForm")
-const altura = document.getElementById("altura")
-const resultado = document.getElementById("resultado")
-imcForm.addEventListener('submit', function(event) {
-   event.preventDefault();
-   calcularIMC(peso, altura) 
-})
-function calcularIMC(p, a){
-    const conta = p/(a*a);
+const peso = document.getElementById("peso");
+const imcForm = document.getElementById("imcForm");
+const altura = document.getElementById("altura");
+const resultado = document.getElementById("resultado");
+imcForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  calcularIMC(peso.value, altura.value);
+});
+function calcularIMC(p, a) {
+  const conta = p / (a*a);
+  let classificacao = "";
+  if (conta < 18.5) {
+    classificacao = "Abaixo do peso";
+  } else if (conta <=24.9) {
+    classificacao = "vnormal";
+  } else if (conta <=29.9) {
+    classificacao = "Sobre peso";
+  } else if (conta <= 34.9) {
+    classificacao = "Obesidade grau I";
+  } else if (conta <= 39.9) {
+    classificacao = "Obesidade grau II";
+  } else {
+    classificacao = "Obesidade grau III";
+  }
+  resultado.textContent=`Seu peso é ${p}, sua altura é ${a}, e seu IMC é ${conta}, seu nível é ${classificacao}`
 
 }
+
